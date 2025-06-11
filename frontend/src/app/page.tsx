8211db8a3364cce3d89700baddcb2e404b148ea5
Skyiesac <jainsachi1202@gmail.com>
2025-06-11 15:30:00 +0530

    Updated the base url for deployment purposes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export default function Home() {
   const [url, setUrl] = useState('');
